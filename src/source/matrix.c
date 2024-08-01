21c469eb0882250be1eb2a67e1a7512f95519706
Rafael de Almeida Innecco <rafael.a.innecco@usp.br>
2024-08-01 08:58:15 -0300

    Change float32x4 to float16x8

--- src/source/matrix.c
+++ src/source/matrix.c
@@ -4,15 +4,15 @@
 #include "matrix.h"
 #include "math_func.h"
 
-float32_t* sum_matrix(float32_t * A, float32_t* B, const int m, const int n) {
+float16_t* sum_matrix(float16_t * A, float16_t* B, const int m, const int n) {
   int i, j;
-  float32_t * C = malloc(sizeof(float32_t)*n*m);
+  float16_t * C = malloc(sizeof(float16_t)*n*m);
   int n_iter = n - n % 4;
   for(i = 0; i < m; i++) {
     for(j = 0; j < n_iter; j = j + 4) {
-      float32x4_t a = vld1q_f32(A + n * i + j);
-      float32x4_t b = vld1q_f32(B + n * i + j);
-      float32x4_t c = vaddq_f32(a, b);
+      float16x8_t a = vld1q_f32(A + n * i + j);
+      float16x8_t b = vld1q_f32(B + n * i + j);
+      float16x8_t c = vaddq_f32(a, b);
       vst1q_f32(C + n*i + j, c);
     }
     
@@ -24,16 +24,16 @@ float32_t* sum_matrix(float32_t * A, float32_t* B, const int m, const int n) {
   return C;
 }
 
-float32x4_t** relu_matrix(const float32x4_t** A, const int m, const int n) {
-  float32x4_t** B = malloc(sizeof(float32x4_t)*n*m);
+float16x8_t** relu_matrix(const float16x8_t** A, const int m, const int n) {
+  float16x8_t** B = malloc(sizeof(float16x8_t)*n*m);
   for(int i = 0; i < m; i ++)
     for(int j = 0; j < n; j++)
       B[i][j] = relu(A[i][j]);
   return B;
 }
 
-float32_t * multiply_matrix_scalar(const float32x4_t** A, const float32_t x, const int m, const int n) {
-  float32_t * B = malloc(sizeof(float32x4_t)*n*m);
+float16_t * multiply_matrix_scalar(const float16x8_t** A, const float16_t x, const int m, const int n) {
+  float16_t * B = malloc(sizeof(float16x8_t)*n*m);
   for(int i = 0; i < m; i ++) {
     for(int j = 0; j < n; j = j + 4) {
       B[i][j] = vmulq_n_f32(A[i][j], x);
@@ -42,9 +42,9 @@ float32_t * multiply_matrix_scalar(const float32x4_t** A, const float32_t x, con
   return B;
 }
 
-float32x4_t** multiply_matrix_matrix(const float32x4_t** A, const float32x4_t** B, const int l,
+float16x8_t** multiply_matrix_matrix(const float16x8_t** A, const float16x8_t** B, const int l,
                                      const int m, const int n) {
-  float32x4_t** C = malloc(sizeof(float32x4_t)*n*m);
+  float16x8_t** C = malloc(sizeof(float16x8_t)*n*m);
   for(int i = 0; i < l; i ++)
     for(int j = 0; j < m; j++) {
       C[i][j] = vmovq_n_f32(0.0);
