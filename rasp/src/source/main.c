e48fe6cfcabcb90664c3a235f19a80b4562104ad
Rafael de Almeida Innecco <rafael.a.innecco@usp.br>
2024-08-14 19:17:33 -0300

    Primeira versão dos testes modificados

--- rasp/src/source/main.c
+++ rasp/src/source/main.c
@@ -6,8 +6,8 @@
 #include "server_fsm.h"
 #include "socket_wrapper.h"
 
-#define MATRIX_M 63
-#define MATRIX_N 63
+#define MATRIX_M 11
+#define MATRIX_N 13
 #define MATRIX_O 9
 
 int main_server();
@@ -127,7 +127,7 @@ int main_test() {
 
   printf("\033[36mMatrizes operando:\n");
   print_matrix(A, MATRIX_M, MATRIX_N);
-  // print_matrix(B, MATRIX_M, MATRIX_N);
+  print_matrix(B, MATRIX_M, MATRIX_N);
   // print_matrix(fvec, MATRIX_M, 1);
   // print_matrix(D, MATRIX_O, MATRIX_N);
   // print_int_matrix(intA, MATRIX_M, 1);
@@ -135,54 +135,72 @@ int main_test() {
   // print_int_matrix(intMA, MATRIX_M, MATRIX_N);
   printf("\033[0m\n");
 
-  // float32_t * scalarA = multiply_matrix_scalar(A, -2.85, MATRIX_M, MATRIX_N);
-  // float32_t * reluA = relu_matrix(A, MATRIX_M, MATRIX_N);
-  // float32_t * C = sum_matrix(A, B, MATRIX_M, MATRIX_N);
-  // float32_t * mult = multiply_matrix_matrix(A, D, MATRIX_M, MATRIX_N,
-  // MATRIX_O); float32_t * diffAB = diff_matrix(A, B, MATRIX_M, MATRIX_N);
-  // float32_t * reluDevA = relu_derivate_matrix(A, MATRIX_M, MATRIX_N);
-  // float32_t * oneHotA = one_hot_matrix(intA, 10, MATRIX_M);
-  // float32_t * transposeB = transpose_matrix(B, MATRIX_M, MATRIX_N);
-  // int * compareAB = compare_vector(intA, intB, MATRIX_M);
+  float32_t* result = malloc(sizeof(float32_t) * MATRIX_M * MATRIX_N);
+  float32_t* resultMult = malloc(sizeof(float32_t) * MATRIX_M * MATRIX_O);
+  float32_t* resultReduxB = malloc(sizeof(float32_t) * MATRIX_M);
+  float32_t* oneHotA = malloc(sizeof(float32_t) * 10 * MATRIX_M);
+  float32_t* resultIntVec = malloc(sizeof(int) * MATRIX_M);
 
-  // sum_multiply_matrix_scalar_fast(altA, B, 5.7, MATRIX_M, MATRIX_N);
+  multiply_matrix_scalar(A, result, -2.85, MATRIX_M, MATRIX_N);
+  printf("\033[33mScalar multiplication A\n");
+  print_matrix(result, MATRIX_M, MATRIX_N);
 
-  float32_t *minMaxA = minmax_matrix(A, MATRIX_M, MATRIX_N);
-  // float32_t * reduxB = matrix_redux_float32_t(B, MATRIX_M, MATRIX_N);
-  // int * reduxIntMA = matrix_redux_int(intMA, MATRIX_M, MATRIX_N);
-  float32_t *sumAvec = sum_matrix_vector(A, fvec, MATRIX_M, MATRIX_N);
-  float32_t *hadamard = multiply_matrix_hadamard(A, B, MATRIX_M, MATRIX_N);
+  relu_matrix(A, result, MATRIX_M, MATRIX_N);
+  printf("\033[34mReLU A\n");
+  print_matrix(result, MATRIX_M, MATRIX_N);
+  
+  sum_matrix(A, B, result, MATRIX_M, MATRIX_N);
+  printf("\033[35mSum A + B\n");
+  print_matrix(result, MATRIX_M, MATRIX_N);
 
-  // printf("\033[33mScalar multiplication A\n");
-  // print_matrix(scalarA, MATRIX_M, MATRIX_N);
-  // printf("\033[34mReLU A\n");
-  // print_matrix(reluA, MATRIX_M, MATRIX_N);
-  // printf("\033[35mSum A + B\n");
-  // print_matrix(C, MATRIX_M, MATRIX_N);
-  // printf("\033[31mdiff A - B\n");
-  // print_matrix(diffAB, MATRIX_M, MATRIX_N);
+  diff_matrix(A, B, result, MATRIX_M, MATRIX_N);
+  printf("\033[31mdiff A - B\n");
+  print_matrix(result, MATRIX_M, MATRIX_N);
+
+  relu_derivate_matrix(A, result, MATRIX_M, MATRIX_N);
+  printf("\033[34mReLU derivate A\n");
+  print_matrix(result, MATRIX_M, MATRIX_N);
+  
+  sum_multiply_matrix_scalar_fast(altA, B, 5.7, MATRIX_M, MATRIX_N);
   // printf("\033[33mA + x * B\n");
   // print_matrix(altA, MATRIX_M, MATRIX_N);
-  // printf("\033[34mReLU derivate A\n");
-  // print_matrix(reluDevA, MATRIX_M, MATRIX_N);
-  // printf("\033[35moneHot A\n");
-  // print_matrix(oneHotA, 10, 5);
-  // printf("\033[31mTraspose B\n");
-  // print_matrix(transposeB, MATRIX_N, MATRIX_M);
+  
+  transpose_matrix(B, result, MATRIX_M, MATRIX_N);
+  printf("\033[31mTraspose B\n");
+  print_matrix(result, MATRIX_N, MATRIX_M);
+
+  minmax_matrix(A, result, MATRIX_M, MATRIX_N);
   printf("\033[33mMinMax A\n");
-  print_matrix(minMaxA, MATRIX_M, MATRIX_N);
-  // printf("\033[34mA x D\n");
-  // print_matrix(mult, MATRIX_M, MATRIX_O);
-  // printf("\033[35mCompare intA intB\n");
-  // print_int_matrix(compareAB, 1, MATRIX_M);
-  // printf("\033[31mredux B\n");
-  // print_matrix(reduxB, MATRIX_M, 1);
-  // printf("\033[33mredux intMA\n");
-  // print_int_matrix(reduxIntMA, MATRIX_M, 1);
-  // printf("\033[34mSum A fvec\n");
-  // print_matrix(sumAvec, MATRIX_M, MATRIX_N);
-  // printf("\033[35mHadamard A B\n");
-  // print_matrix(hadamard, MATRIX_M, MATRIX_N);
+  print_matrix(result, MATRIX_M, MATRIX_N);
+
+  multiply_matrix_matrix(A, D, MATRIX_M, MATRIX_N, MATRIX_O); 
+  printf("\033[34mA x D\n");
+  print_matrix(resultMult, MATRIX_M, MATRIX_O);
+
+  matrix_redux_float(B, resultReduxB, MATRIX_M, MATRIX_N);
+  printf("\033[31mredux B\n");
+  print_matrix(resultReduxB, MATRIX_M, 1);
+
+  one_hot_matrix(intA, 10, MATRIX_M);
+  printf("\033[35moneHot A\n");
+  print_matrix(oneHotA, 10, Matrix_M);
+
+  multiply_matrix_hadamard(A, B, result, MATRIX_M, MATRIX_N);
+  printf("\033[35mHadamard A B\n");
+  print_matrix(result, MATRIX_M, MATRIX_N);
+
+  int * compareAB = compare_vector(intA, intB, resultIntVec,  MATRIX_M);
+  printf("\033[35mCompare intA intB\n");
+  print_int_matrix(resultIntVec, MATRIX_M, 1);
+
+  int * reduxIntMA = matrix_redux_int(intMA, resultIntVec, MATRIX_M, MATRIX_N);
+  printf("\033[33mredux intMA\n");
+  print_int_matrix(resultIntVec, MATRIX_M, 1);
+
+  sum_matrix_vector(A, fvec, result, MATRIX_M, MATRIX_N);
+  printf("\033[34mSum A fvec\n");
+  print_matrix(sumAvec, result, MATRIX_M, MATRIX_N);
+
   printf("\033[0m");
 
   free(A);
@@ -194,9 +212,5 @@ int main_test() {
   free(intB);
   free(intMA);
 
-  free(minMaxA);
-  free(sumAvec);
-  free(hadamard);
-  // free(transposeB);
   return 0;
 }
